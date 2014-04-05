8e75165413531f66948afd5c71cbbaa4f820e0f4
Carrigan <brian.c.carrigan@gmail.com>
2014-04-05 18:34:17 -0400

    Wireless commo working

--- roampy/backend.py
+++ roampy/backend.py
@@ -36,11 +36,6 @@ STATE_DISCONNECTED = 0
 STATE_CONNECTED = 1
 STATE_WAITING_FOR_LINK = 2
 
-states = {STATE_DISCONNECTED: "DISCONNECTED",
-          STATE_CONNECTED: "CONNECTED",
-          STATE_WAITING_FOR_LINK: "WAITING FOR LINK",
-          }
-
 class roam_app(object):
         def __init__(self, size, com):
                 self.comport = com
@@ -51,9 +46,14 @@ class roam_app(object):
                 self.queue = []
                 self.status = STATE_DISCONNECTED
                 self.connect()
+                self.kill = False
 
         @property
         def state(self):
+                states = {STATE_DISCONNECTED: "DISCONNECTED",
+                          STATE_CONNECTED: "CONNECTED",
+                          STATE_WAITING_FOR_LINK: "WAITING FOR LINK",
+                          }
                 return states[self.status]
 
         def __exit__(self):
@@ -61,14 +61,16 @@ class roam_app(object):
 
         def connect(self):
                 if not self.xbee:
+                        self.kill = False
                         self.ser = serial.Serial(self.comport, 115200)
                         self.xbee = ZigBee(self.ser, callback=self.receive_handler)
                         print "App started on " + self.comport + "..."
+                        self.process()
 
         def disconnect(self):
                 if self.xbee:
                         self.scanning = False
-                        
+                        self.kill = True
                         self.xbee.halt()
                         self.ser.close()
                         self.xbee = None
@@ -76,20 +78,32 @@ class roam_app(object):
                         print "App ended."
 
         def send(self, data):
+                self.response = False
                 if not self.xbee:
                         self.connect()
                 self.xbee.send('tx', dest_addr_long = self.dest, dest_addr = "\xFF\xFE", data = data)
+                while not self.response:
+                        pass
 
         def link(self):
-                self.send('$')
                 self.status = STATE_WAITING_FOR_LINK
-
+                self.send('$')
+                
         def process(self):
                 for entry in self.queue:
                         self.state_handle(entry)
                 self.queue = []
 
+                if not self.kill:
+                        threading.Timer(.02, self.process).start()
+
         def state_handle(self, packet):
+                if not packet:
+                        return
+                
+                if packet['id'] == 'tx_status':
+                        self.response = True
+        
                 if self.status == STATE_WAITING_FOR_LINK:
                         if packet['id'] == 'tx_status':
                                 status = packet['deliver_status']
@@ -97,6 +111,13 @@ class roam_app(object):
                                         self.status = STATE_CONNECTED
                                 else:
                                         self.status = STATE_DISCONNECTED
+                                return
+
+                if packet['id'] == 'at_response' and packet['frame_id'] == "A":
+                        self.rssi = ord(packet['parameter'])
+                        return
+                
+                print packet
 
         def receive_handler(self, data):
                 self.queue.append(data)
@@ -112,8 +133,6 @@ class roam_app(object):
                 if not self.scanning:
                         return
                 
-                self.process()
-                
                 if self.status == STATE_DISCONNECTED:
                         self.link()
                         
@@ -122,8 +141,27 @@ class roam_app(object):
                 else:
                         print "No device found. Waiting..."
                         threading.Timer(1, self.scan_handler).start()                
-                
-                
+
+        def get_last_rssi(self):
+                self.rssi = None
+                self.xbee.at(frame_id='A', command="DB")
+                while not self.rssi:
+                        pass
+                return self.rssi
+
+        def start_alarm(self):
+                self.send("N")
+
+        def end_alarm(self):
+                self.send("P")
+
+        def alarm_for(self, seconds):
+                self.start_alarm()
+                threading.Timer(seconds, self.end_alarm).start()
 
 roam = roam_app(10, 'COM3')
+roam.scan()
+roam.send("N\r\n")
+roam.get_last_rssi()
+
 
